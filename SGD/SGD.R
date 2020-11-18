285ffcd40cefa586f93f2ee86e93584f6709fbd5
Rediskos <danil0662015046@gmail.com>
2020-11-18 16:41:00 +0300

    SGD done

--- SGD/SGD.R
+++ SGD/SGD.R
@@ -1,7 +1,7 @@
 
 
-#функция стозастического градиента
-SDG <- function(xl, learn_temp, lyambda, los_func, los_func_deriv) {
+#функция стохастического градиентного спуска
+SGD <- function(xl, learn_temp, lyambda, los_func, los_func_deriv) {
   #xl - датафрейм векторов выборки
   #learn_temp - темп обучения
   #lyambda - параметр сглаживания
@@ -9,8 +9,8 @@ SDG <- function(xl, learn_temp, lyambda, los_func, los_func_deriv) {
   #los_func_deriv - производная функции потерь
   
   l <- dim(xl)[1] #размер выборки
-  x <- xl[, 1:l-1] #датафрейм параметров
-  y <- xl[, l] #датафрейм ответов
+  x <- as.matrix(xl[, 1:l-1], ncol = l-1) #матрица параметров
+  y <- as.matrix(xl[, l], ncol = 1) #матрица ответов
   m <- dim(x)[2] #сколько типов признаков 
   
   w <- runif(m, -1/(2*l), 1/(2*l)) #начальные веса
@@ -22,21 +22,25 @@ SDG <- function(xl, learn_temp, lyambda, los_func, los_func_deriv) {
     xi <- x[i,]
     yi <- y[i,]
     
-    for_los_func <- w %*% t(xi) * yi
+    t_xi <- t(xi)
+    
+    for_los_func <- w %*% t_xi * yi
     
     los_func_res <- los_func(for_los_func)
     
     Q <- Q + los_func_res
   }
   
-  Q_past <- Q + 10000
+  Q_past <- Q + 10000#значение ошибки с предыдущего шага.
   
   while (abs(Q_past - Q) > 1e-4) {
     rand_indx <- sample(1:l, 1)
     xi <- x[rand_indx,]
-    yi <- x[rand_indx,]
+    yi <- y[rand_indx,]
     
-    for_los_func <- w %*% t(xi) * yi
+    t_xi <- t(xi)
+    
+    for_los_func <- w %*% t_xi * yi
     res_from_los <- los_func(for_los_func)
     
     res_from_los_deriv <- los_func_deriv(for_los_func)
@@ -49,4 +53,7 @@ SDG <- function(xl, learn_temp, lyambda, los_func, los_func_deriv) {
     Q <- (1 - lyambda) * Q + lyambda * res_from_los
   }
   
+  
+  return(w)
 }
+
